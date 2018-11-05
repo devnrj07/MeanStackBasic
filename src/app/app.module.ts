89aa9cd5ac3aac5b4bec4b9f57bf23b3d5d519a9
NRJ <Neeraj.Mukta@NIIT-Tech.com>
2018-11-05 13:04:12 +0530

    V2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+import { BrowserModule } from '@angular/platform-browser';  
+import { NgModule } from '@angular/core';   
+  
+import { HttpModule } from '@angular/http';  
+import { FormsModule } from '@angular/forms';  
+  
+import { AppComponent } from './app.component';  
+  
+import {CommonService} from './common.service';  
+  
+  
+@NgModule({  
+  declarations: [  
+    AppComponent  
+  ],  
+  imports: [  
+    BrowserModule,HttpModule,FormsModule,  
+  ],  
+  providers: [CommonService],  
+  bootstrap: [AppComponent]  
+})  
+export class AppModule { }  
